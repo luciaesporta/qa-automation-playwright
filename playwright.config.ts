cb665e6c822334da93dc4528304ce63fb53b9fe2
Lucía Esporta <luciaalvarezesporta@gmail.com>
2025-09-20 19:56:20 -0300

    fix in ci/cd implementation

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   testDir: './tests',
   reporter: 'html',
   use: {
-    baseURL: process.env.BASE_URL ?? 'http://localhost:3000',
+    //baseURL: process.env.BASE_URL ?? 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
   },
 
